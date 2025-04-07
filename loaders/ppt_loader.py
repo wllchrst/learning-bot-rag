98c0b565eac0df132d962a2b136cfa352b19ecab
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-04-07 21:29:57 +0700

    feat: add documentation

--- loaders/ppt_loader.py
+++ loaders/ppt_loader.py
@@ -1,6 +1,12 @@
 from langchain.document_loaders import UnstructuredPowerPointLoader
 
 def load_ppt(temp_file_path: str, material_code: str):
+    """Load PPT and material code, so it can be saved in vector database.
+
+    Args:
+        temp_file_path (str): temporary file path that is used for saving the ppt file
+        material_code (str): material code for the session detail
+    """
     loader = UnstructuredPowerPointLoader(temp_file_path)
     documents = loader.load()
 
