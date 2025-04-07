e65eba32aaad6ba79df4f408e3dac4d3bab5b1f3
William Christian <wllchrst@outlook.com>
2025-04-07 16:51:45 +0700

    feat: ppt loader uses material_code

--- loaders/ppt_loader.py
+++ loaders/ppt_loader.py
@@ -1,8 +1,9 @@
 from langchain.document_loaders import UnstructuredPowerPointLoader
 
-def load_ppt(temp_file_path):
+def load_ppt(temp_file_path: str, material_code: str):
     loader = UnstructuredPowerPointLoader(temp_file_path)
     documents = loader.load()
 
+    print(material_code)
     for doc in documents:
         print(doc.page_content)
