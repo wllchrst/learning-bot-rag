91fb5b8f2ebc971d29f2a919848bbb029566181c
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-04-07 21:29:23 +0700

    feat: added documentation and uses env helper

--- database/collections_script/create_script.py
+++ database/collections_script/create_script.py
@@ -1,5 +1,11 @@
-from .session_detail import SessionDetailCollection
+"""Script for creating all collection"""
 from database import DatabaseClient
+from database.collections_script import SessionDetailCollection
+from helpers import EnvHelper
 def create_collections():
+    """Script for create all collection that is needed for the application """
+    envhelper = EnvHelper()
     database_client = DatabaseClient()
+
+    database_client.list_collections(envhelper.DATABASE_NAME)
     SessionDetailCollection(client=database_client.client)
