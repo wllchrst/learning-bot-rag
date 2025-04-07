1052c39a286aec8fa35efb2a551631b07c817345
nosurprisesplz <129862579+wllchrst@users.noreply.github.com>
2025-04-07 21:29:37 +0700

    fix: wrong index name

--- database/collections_script/session_detail.py
+++ database/collections_script/session_detail.py
@@ -1,11 +1,13 @@
-from database.collections_script.template import CollectionCreateTemplate
 from pymilvus import MilvusClient, DataType
+from database.collections_script.template import CollectionCreateTemplate
+from helpers import EnvHelper
 class SessionDetailCollection(CollectionCreateTemplate):
     def __init__(self, client: MilvusClient):
+        envhelper = EnvHelper()
         super().__init__(
             client=client,
-            collection_name="session_details",
-            database_name="session_data"
+            collection_name=envhelper.SESSION_DATA_COLLECTION,
+            database_name=envhelper.DATABASE_NAME
         )
 
     def create_schema(self):
@@ -13,35 +15,35 @@ class SessionDetailCollection(CollectionCreateTemplate):
             auto_id=False,
             enable_dynamic_field=True,
         )
-        
+
         self.schema.add_field(
             field_name="detail_id",
             datatype=DataType.INT64,
             is_primary=True
         )
-        
+
         self.schema.add_field(
             field_name="vector",
             datatype=DataType.FLOAT_VECTOR,
             dim=5
         )
-        
+
         self.schema.add_field(
             field_name="detail_text",
             datatype=DataType.VARCHAR,
             max_length=10000
         )
-        
+
     def create_indexes(self):
         self.index_params = self.client.prepare_index_params()
 
         self.index_params.add_index(
-            field_name="my_id",
+            field_name="detail_id",
             index_type="AUTOINDEX"
         )
 
         self.index_params.add_index(
-            field_name="my_vector", 
+            field_name="vector",
             index_type="AUTOINDEX",
             metric_type="COSINE"
         )
