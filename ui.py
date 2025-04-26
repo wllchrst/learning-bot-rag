5640d59848e1a2f7c4c973b618c6ffc9f4d078dd
William Christian <wllchrst@outlook.com>
2025-04-26 09:36:17 +0700

    feat: UI Chat for applicatoin

---
+++ ui.py
@@ -0,0 +1,29 @@
+import streamlit as st
+from ai.chat import LangchainChat
+
+chat = LangchainChat()
+
+st.title("Learning Chat Bot RAG")
+
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+if prompt := st.chat_input("Anything I can help you with?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    chat_response = chat.answer(prompt)
+    print(chat_response)
+    response = chat_response.answer
+
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
