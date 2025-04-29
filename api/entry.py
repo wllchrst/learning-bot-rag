4294f2de5e17287883e8284d6befe95918eb3415
William Christian <wllchrst@outlook.com>
2025-04-29 08:11:17 +0700

    fix: cannot serialize json answer response

--- api/entry.py
+++ api/entry.py
@@ -1,11 +1,13 @@
 import uvicorn
 import tempfile
 import os
+import traceback
 from fastapi import FastAPI, File, UploadFile, HTTPException, Form, Depends
 from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from fastapi.middleware.cors import CORSMiddleware
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from dataclasses import asdict
 from api.handlers import data_handler, llm_handler
 from api.security import verify_api_key
 from api.utils import format_response  # Import the utility function
@@ -62,8 +64,9 @@ async def ask_chatbot(question: str, chat_id: str):
     chat_id = chat_id.strip()
     try:
         response = llm_handler.ask_question(question, chat_id)
-        return format_response(data={"response": response}, message="Question processed successfully")
+        return format_response(data={"response": asdict(response)}, message="Question processed successfully")
     except Exception as e:
+        traceback.print_exc()
         return format_response(data=None, message=f"Error getting question from LLM: {str(e)}", success=False, status_code=500)
 
 def start_api():
--- api/handlers/llm_handler.py
+++ api/handlers/llm_handler.py
@@ -1,11 +1,12 @@
 """Handler file that is focused to accept everything related to llm answer question, changin llm settings and other."""
 from ai.chat import LangchainChat
 from helpers import ProfanityHelper
+from models.data.answer_response import AnswerResponse
 
 langchain_chat = LangchainChat()
 profanity_helper = ProfanityHelper()
 
-def ask_question(question: str, chat_id: str) -> str:
+def ask_question(question: str, chat_id: str) -> AnswerResponse:
     result = profanity_helper.check(question)
     if result:
         raise ValueError("Profanity detected in the question., please rephrase it.")
