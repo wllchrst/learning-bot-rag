31a9228b9be8885bc746425c50b1dfa1a8c30408
William Christian <wllchrst@outlook.com>
2025-04-29 08:11:31 +0700

    chore: example for state

--- models/interfaces/state.py
+++ models/interfaces/state.py
@@ -1,6 +1,19 @@
 from typing_extensions import TypedDict, List
 from models.interfaces.chat_history import ChatHistory
 
+# state = State(
+#         answer='',
+#         chat_history=[
+#                 {
+#                     "answer": "",
+#                     "chat_id": "chat id",
+#                     'question': "What is the rectifier?"
+#                 }
+#             ],
+#         chat_id='',
+#         question='What is the function then?',
+#         context=''
+#     )
 class State(TypedDict):
     chat_id: str
     context: List[str]
